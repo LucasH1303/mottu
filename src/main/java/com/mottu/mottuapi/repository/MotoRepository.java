85451bbd929634559c6e7279b761f89d90c8c733
LucasH1303 <lucas06henrique@outlook.com>
2025-05-25 15:35:47 -0300

    Configuração do Banco de Dados

--- src/main/java/com/mottu/mottuapi/repository/MotoRepository.java
+++ src/main/java/com/mottu/mottuapi/repository/MotoRepository.java
@@ -4,4 +4,6 @@ import com.mottu.mottuapi.entity.Moto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MotoRepository extends JpaRepository<Moto, Long> {
+
+	void deleteById(Long id);
 }
