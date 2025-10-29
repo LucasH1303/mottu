0a026571837f6ae5426743f056cab4f4b3b8b4ec
LucasH1303 <150475388+LucasH1303@users.noreply.github.com>
2025-10-29 02:06:18 -0300

    Update create-sql-server.ps1

--- create-sql-server.ps1
+++ create-sql-server.ps1
@@ -1,9 +1,9 @@
-$RG = "rg-testesprint4"
+$RG = "rg-Decepticons_CP6"
 $LOCATION = "brazilsouth"
-$SERVER_NAME = "sqlserver-testesprint4"
+$SERVER_NAME = "sqlserver-Decepticons_CP6"
 $USERNAME = "admsql"
 $PASSWORD = "Fiap@2tdsvms"
-$DBNAME = "testedb"
+$DBNAME = "Decepticonsdb"
 
 az group create --name $RG --location $LOCATION
 az sql server create -l $LOCATION -g $RG -n $SERVER_NAME -u $USERNAME -p $PASSWORD --enable-public-network true
